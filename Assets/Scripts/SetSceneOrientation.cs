ae08c69e4f4a9ab37a1303962864fa3e20653fa8
Lemachu <alemahusic@gmail.com>
2023-09-19 13:04:30 +0200

    drugi

--- Assets/Scripts/SetSceneOrientation.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public class SetSceneOrientation : MonoBehaviour {
-
-    void Awake () {
-        Screen.orientation = ScreenOrientation.LandscapeLeft;
-    }
-}
